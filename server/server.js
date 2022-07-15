eab742d76613a23a33fd24b04bb82dfe5e6366d4
Hulkaroy Jouraboeva <86935670+hulkaroyJouraboeva@users.noreply.github.com>
2022-07-15 19:02:32 -0400

    basic boiler plate code setup & configured webpack
    Co-authored-by: kngreen <kngreen@users.noreply.github.com>
    Co-authored-by: j-hickey <j-hickey@users.noreply.github.com>
    Co-authored-by: Ahsan Rao <ahsan-rao@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
+
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+  });
+};
+
+
+// app.use((req, res) => res.sendStatus(404)); // catch-all route handler for any requests to an unknown route
+app.get('*', (req, res) => {
+  console.log("Invalid URL detected");
+  res.status(404).json({ error: `Page not found, request to ${req.path} failed` });
+});
+
+
+/**
+ * configure express global error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+ app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred: ' + err }, 
+  };
+  
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.message);
+  
+  return res.send({'Error status': errorObj.status, 'Message': errorObj.message});
+  // return res.status(errorObj.status).json(errorObj.message)
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
