2f52c59747dc7702c1a5567265a7b9c3352c89f7
kngreen <kngreen@umich.edu>
2022-07-15 19:20:30 -0400

    branch testing

--- client/App.jsx
+++ client/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 
   return (
     <div className="App">
-        <div>HEY</div>
+        <div>Update</div>
       {/* <BrowserRouter>
         <Routes> */}
           {/* <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
